6d156fb272ff341d729e55107b4735efeb2c5d76
’Jarkko <jm60697@gmail.com>
2010-04-17 11:27:15 +0300

    First runnable (but butt ugly) RoR version of server

--- server/ror/catss/app/controllers/devices_controller.rb
+++ server/ror/catss/app/controllers/devices_controller.rb
@@ -1,35 +1,17 @@
 class DevicesController < ApplicationController
   # GET /devices
-  # GET /devices.xml
   def index
-    @devices = Device.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @devices }
-    end
+    @devices = Device.scoped
   end
 
   # GET /devices/1
-  # GET /devices/1.xml
   def show
     @device = Device.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @device }
-    end
   end
 
   # GET /devices/new
-  # GET /devices/new.xml
   def new
     @device = Device.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @device }
-    end
   end
 
   # GET /devices/1/edit
@@ -38,46 +20,32 @@ class DevicesController < ApplicationController
   end
 
   # POST /devices
-  # POST /devices.xml
   def create
     @device = Device.new(params[:device])
 
-    respond_to do |format|
-      if @device.save
-        format.html { redirect_to(@device, :notice => 'Device was successfully created.') }
-        format.xml  { render :xml => @device, :status => :created, :location => @device }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @device.errors, :status => :unprocessable_entity }
-      end
+    if @device.save
+      redirect_to(@device, :notice => 'Device was successfully created.') 
+    else
+      render :action => "new" 
     end
   end
 
-  # PUT /devices/1
-  # PUT /devices/1.xml
+  # PUT /devices/
   def update
     @device = Device.find(params[:id])
 
-    respond_to do |format|
-      if @device.update_attributes(params[:device])
-        format.html { redirect_to(@device, :notice => 'Device was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @device.errors, :status => :unprocessable_entity }
-      end
+    if @device.update_attributes(params[:device])
+        redirect_to(@device, :notice => 'Device was successfully updated.') 
+    else
+        render :action => "edit" 
     end
   end
 
   # DELETE /devices/1
-  # DELETE /devices/1.xml
   def destroy
     @device = Device.find(params[:id])
     @device.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(devices_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(devices_url) 
   end
 end
--- server/ror/catss/db/schema.rb
+++ server/ror/catss/db/schema.rb
@@ -9,6 +9,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20100417075445) do
+
+  create_table "devices", :force => true do |t|
+    t.string   "name"
+    t.text     "imei"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
