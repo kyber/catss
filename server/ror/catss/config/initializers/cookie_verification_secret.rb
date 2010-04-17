e191159e13459f589189c980d8f8d333bcacd414
’Jarkko <jm60697@gmail.com>
2010-04-17 09:54:41 +0300

    Added Rails code for Server

---
+++ server/ror/catss/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.cookie_secret = '306d89eec1ba996d9f873617e36a86ff514500d87112e9fdca280982c7d539cb1bd3c27c246be51cf6f9ab139ce5671d91d97d799246f202f3e95716a978896f'
