f7a79cf2a666755d72422563b464857e6b2c82c8
’Jarkko <jm60697@gmail.com>
2010-04-22 18:57:27 +0300

    Added more i18n translations...

--- server/ror/catss/app/controllers/application_controller.rb
+++ server/ror/catss/app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
    I18n.locale = params[:locale]
  
     #if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
-    #  cookies['locale'] = { :value => params[:locale], :expires => 1.year.from_now }
+ #  cookies[:locale] = { :value => params[:locale], :expires => 1.year.from_now }
     #  I18n.locale = params[:locale].to_sym
     #elsif cookies['locale'] && I18n.available_locales.include?(cookies['locale'].to_sym)
     #  I18n.locale = cookies['locale'].to_sym
